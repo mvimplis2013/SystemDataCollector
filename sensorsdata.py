613aefa1d176656d716df0f9913407a8a256d40c
Miltos69 <mvimblis@gmail.com>
2018-03-16 23:27:15 +0200

    sensor data again

--- sensorsdata.py
+++ sensorsdata.py
@@ -1,6 +1,27 @@
+import json
+
 class SensorsData:
     def __init__(self, data):
         self.data = data
 
     def getData(self):
-        print(self.data)
+        data = self.data
+        if isinstance(data, list):
+            print("list")
+        elif isinstance(data, str): 
+            print("string")
+
+        data2 = data[1:-1].split("}")
+        print( " ... %d" % len(data2) )
+        print("&&&1" + data2[0])
+        print("&&&2" + data2[1])
+        print("&&&" + data2[2])
+
+        print( "wwww " + data[1:-1])
+        d1 = json.loads(data2[0]+"}")
+        ds = data2[1]
+        d2 = json.loads(ds[1:]+"}")
+        print("****", d1["type"])
+        print("****", d2["value"])
+        
+        print(data)
